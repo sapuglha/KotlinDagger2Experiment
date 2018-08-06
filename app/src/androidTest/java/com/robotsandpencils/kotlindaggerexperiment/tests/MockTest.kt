f2a8d8b8417581bae91c877c0b66106a1759f80e
Tiago Silva <tiago.silva@robotsandpencils.com>
2018-08-06 00:15:00 -0600

    Consolidate and update libs versions

--- app/src/androidTest/java/com/robotsandpencils/kotlindaggerexperiment/tests/MockTest.kt
+++ app/src/androidTest/java/com/robotsandpencils/kotlindaggerexperiment/tests/MockTest.kt
@@ -1,9 +1,9 @@
 package com.robotsandpencils.kotlindaggerexperiment.tests
 
-import com.nhaarman.mockito_kotlin.anyOrNull
-import com.nhaarman.mockito_kotlin.eq
-import com.nhaarman.mockito_kotlin.mock
-import com.nhaarman.mockito_kotlin.whenever
+import com.nhaarman.mockitokotlin2.anyOrNull
+import com.nhaarman.mockitokotlin2.eq
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.whenever
 import com.robotsandpencils.kotlindaggerexperiment.App
 import com.robotsandpencils.kotlindaggerexperiment.TestHelper
 import com.robotsandpencils.kotlindaggerexperiment.app.managers.PreferencesManager
